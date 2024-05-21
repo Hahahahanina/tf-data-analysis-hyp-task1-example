6df3dd6ac706839b01c26cac6be17a5318fd4943
Katya Khanina <47501502+Hahahahanina@users.noreply.github.com>
2024-05-21 11:45:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1260358058 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    mean_control = x_success / x_cnt
+    mean_test = y_success / y_cnt
+    alpha = 0.01 
+    test = sps.norm(loc=mean_test).rvs(1000)
+    control = sps.norm(loc=mean_control).rvs(1000)
+    pvalue = sps.ttest_ind(test, control, alternative='less').pvalue
+    return pvalue > alpha
